f278a2d2092b30b7333ebc66f29191937f0cd51c
Vladimir <shaydurov20@gmail.com>
2022-08-21 22:39:49 +0300

    add debug mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import eruda from 'eruda'
-
 import { App } from 'components/layout/App'
 
 // import '@vkontakte/vkui/dist/components.css'
@@ -12,12 +10,6 @@ import '@vkontakte/vkui/dist/unstable.css'
 import '@vkontakte/vkui/dist/fonts.css'
 
 import 'styles/index.css'
-
-eruda.init()
-
-if (process.env.NODE_ENV !== 'production') {
-   eruda.remove('console')
-}
 
 ReactDOM.render(
    <React.StrictMode>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,26 @@
 import { startGlobalRouter } from '@happysanta/router'
+import type { Eruda } from 'eruda'
 import { routes } from './routes'
 
 export const router = startGlobalRouter(routes)
+
+let eruda: Eruda | null = null
+
+router.addListener('update', (newRoute, oldRoute, isNew, type) => {
+   if (Boolean(newRoute.params['debug']) === true) {
+      if (eruda === null) {
+         import('eruda').then((module) => {
+            eruda = module.default
+            eruda.init()
+         })
+      } else {
+         eruda.init()
+      }
+   } else if (
+      Boolean(newRoute.params['debug']) === false &&
+      Boolean(oldRoute?.params['debug']) === true &&
+      eruda !== null
+   ) {
+      eruda.destroy()
+   }
+})
